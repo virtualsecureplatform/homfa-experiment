2f4cd5cce9b8fe55848632651874666723c5ddb4
Naoki Matsumoto <naoki@pibvt.net>
2021-08-14 04:40:01 +0900

    Add benchmark for 'towards'

--- bench.sh
+++ bench.sh
@@ -3,15 +3,74 @@
 BUILD_BIN=${BUILD_BIN:-"build/bin"}
 BENCHMARK=$BUILD_BIN/benchmark
 
-$BENCHMARK offline --spec damon-001.spec --in adult-001-bg.in --bootstrapping-freq 30000 --ap 5  > offline-damon-001.log
-$BENCHMARK offline --spec damon-002.spec --in adult-001-dbg.in --bootstrapping-freq 30000 --ap 5 > offline-damon-002.log
-$BENCHMARK offline --spec damon-004.spec --in adult-001-bg.in --bootstrapping-freq 30000 --ap 5  > offline-damon-004.log
-$BENCHMARK offline --spec damon-005.spec --in adult-001-bg.in --bootstrapping-freq 30000 --ap 5  > offline-damon-005.log
-$BENCHMARK reversed --spec damon-001.spec --in adult-001-bg.in --bootstrapping-freq 30000 --out-freq 15 --ap 5  > reversed-damon-001.log
-$BENCHMARK reversed --spec damon-002.spec --in adult-001-dbg.in --bootstrapping-freq 30000 --out-freq 15 --ap 5 > reversed-damon-002.log
-$BENCHMARK reversed --spec damon-004.spec --in adult-001-bg.in --bootstrapping-freq 30000 --out-freq 15 --ap 5 > reversed-damon-004.log
-$BENCHMARK reversed --spec damon-005.spec --in adult-001-bg.in --bootstrapping-freq 30000 --out-freq 15 --ap 5 > reversed-damon-005.log
-$BENCHMARK qtrlwe2 --spec damon-001.spec --in adult-001-bg.in --bootstrapping-freq 1 --out-freq 15 --ap 5 --queue-size 15 --max_second_lut_depth 8 > qtrlwe2-damon-001.log
-$BENCHMARK qtrlwe2 --spec damon-002.spec --in adult-001-dbg.in --bootstrapping-freq 1 --out-freq 15 --ap 5 --queue-size 15 --max_second_lut_depth 8 > qtrlwe2-damon-002.log
-$BENCHMARK qtrlwe2 --spec damon-004.spec --in adult-001-bg.in --bootstrapping-freq 1 --out-freq 15 --ap 5 --queue-size 15 --max_second_lut_depth 8 > qtrlwe2-damon-004.log
-$BENCHMARK qtrlwe2 --spec damon-005.spec --in adult-001-bg.in --bootstrapping-freq 1 --out-freq 15 --ap 5 --queue-size 15 --max_second_lut_depth 8 > qtrlwe2-damon-005.log
+failwith(){
+    echo -ne "\e[1;31m[ERROR]\e[0m "
+    echo "$1"
+    exit 1
+}
+
+run_benchmark(){
+    case "$1" in
+        "offline" )
+	    $BENCHMARK offline --spec $2 --in $3 --bootstrapping-freq 30000 --ap 5  > $1_$2_$3.log
+            ;;
+        "reversed" )
+            $BENCHMARK reversed --spec $2 --in $3 --bootstrapping-freq 30000 --out-freq 15 --ap 5  > $1_$2_$3.log
+            ;;
+        "qtrlwe2" )
+	    $BENCHMARK qtrlwe2 --spec $2 --in $3 --bootstrapping-freq 1 --out-freq 15 --ap 5 --queue-size 15 --max-second-lut-depth 8 > $1_$2_$3.log
+            ;;
+        "plain" )
+	    $BENCHMARK plain --spec $2 --in $3 --out-freq 15 --ap 5 > $1_$2_$3.log
+            ;;
+        * )
+            failwith "Invalid run $1"
+            ;;
+    esac
+}
+
+run_benchmark plain    damon-001.spec adult-001-bg.in
+run_benchmark offline  damon-001.spec adult-001-bg.in
+run_benchmark reversed damon-001.spec adult-001-bg.in
+run_benchmark qtrlwe2  damon-001.spec adult-001-bg.in
+
+run_benchmark plain    damon-002.spec adult-001-dbg.in
+run_benchmark offline  damon-002.spec adult-001-dbg.in
+run_benchmark reversed damon-002.spec adult-001-dbg.in
+run_benchmark qtrlwe2  damon-002.spec adult-001-dbg.in
+
+run_benchmark plain    damon-004.spec adult-001-bg.in
+run_benchmark offline  damon-004.spec adult-001-bg.in
+run_benchmark reversed damon-004.spec adult-001-bg.in
+run_benchmark qtrlwe2  damon-004.spec adult-001-bg.in
+
+run_benchmark plain    damon-005.spec adult-001-bg.in
+run_benchmark offline  damon-005.spec adult-001-bg.in
+run_benchmark reversed damon-005.spec adult-001-bg.in
+run_benchmark qtrlwe2  damon-005.spec adult-001-bg.in
+
+run_benchmark plain    towards-001.spec adult-001-dt-30-bg.in
+run_benchmark offline  towards-001.spec adult-001-dt-30-bg.in
+run_benchmark reversed towards-001.spec adult-001-dt-30-bg.in
+run_benchmark qtrlwe2  towards-001.spec adult-001-dt-30-bg.in
+
+run_benchmark plain    towards-002.spec adult-001-dt-30-bg.in
+run_benchmark offline  towards-002.spec adult-001-dt-30-bg.in
+run_benchmark reversed towards-002.spec adult-001-dt-30-bg.in
+run_benchmark qtrlwe2  towards-002.spec adult-001-dt-30-bg.in
+
+run_benchmark plain    towards-004.spec adult-001-dt-30-bg.in
+run_benchmark offline  towards-004.spec adult-001-dt-30-bg.in
+run_benchmark reversed towards-004.spec adult-001-dt-30-bg.in
+run_benchmark qtrlwe2  towards-004.spec adult-001-dt-30-bg.in
+
+run_benchmark plain    towards-005.spec adult-001-dt-30-bg.in
+run_benchmark offline  towards-005.spec adult-001-dt-30-bg.in
+run_benchmark reversed towards-005.spec adult-001-dt-30-bg.in
+run_benchmark qtrlwe2  towards-005.spec adult-001-dt-30-bg.in
+
+run_benchmark plain    towards-006.spec adult-001-dt-30-bg.in
+run_benchmark offline  towards-006.spec adult-001-dt-30-bg.in
+run_benchmark reversed towards-006.spec adult-001-dt-30-bg.in
+run_benchmark qtrlwe2  towards-006.spec adult-001-dt-30-bg.in
+
