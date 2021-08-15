2c4f0a826d7ec6d758b4f667e4abeebded090236
Naoki Matsumoto <naoki@pibvt.net>
2021-08-15 22:01:24 +0900

    Add damon tests

---
+++ ap6/gen_damon_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ruby gen_test_damon-001.rb true 10080  | ruby 01tobin.rb 6 > test-damon-001-true.in
+ruby gen_test_damon-001.rb false 10080 | ruby 01tobin.rb 6 > test-damon-001-false.in
+ruby gen_test_damon-002.rb true 10080  | ruby 01tobin.rb 6 > test-damon-002-true.in
+ruby gen_test_damon-002.rb false 10080 | ruby 01tobin.rb 6 > test-damon-002-false.in
+ruby gen_test_damon-004.rb true 10080  | ruby 01tobin.rb 6 > test-damon-004-true.in
+ruby gen_test_damon-004.rb false 10080 | ruby 01tobin.rb 6 > test-damon-004-false.in
+ruby gen_test_damon-005.rb true 10080  | ruby 01tobin.rb 6 > test-damon-005-true.in
+ruby gen_test_damon-005.rb false 10080 | ruby 01tobin.rb 6 > test-damon-005-false.in
