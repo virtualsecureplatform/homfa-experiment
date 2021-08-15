9a32fcd30c88900afe9a242e027ac3fcaf1835a4
Ushitora Anqou <ushitora@anqou.net>
2021-08-16 00:00:57 +0900

    Update check_result_correct.sh

--- ap6/check_result_correct.sh
+++ ap6/check_result_correct.sh
@@ -1,10 +1,52 @@
 #!/usr/bin/bash -eu
 
 cd "$1"
-diff \
-    <(grep "result" plain_towards-001.spec_adult-001-night-bg.in.log) \
-    <(grep "result" qtrlwe2_towards-001.spec_adult-001-night-bg.in.log)
-diff \
-    <(grep "result" plain_towards-001.spec_adult-001-night-bg.in.log) \
-    <(grep "result" reversed_towards-001-rev.spec_adult-001-night-bg.in.log)
 
+if [ -f qtrlwe2_towards-001.spec_adult-001-night-bg.in.log ]; then
+    echo -n "."
+    diff \
+        <(grep "result" plain_towards-001.spec_adult-001-night-bg.in.log) \
+        <(grep "result" qtrlwe2_towards-001.spec_adult-001-night-bg.in.log)
+fi
+if [ -f reversed_towards-001.spec_adult-001-night-bg.in.log ]; then
+    echo -n "."
+    diff \
+        <(grep "result" plain_towards-001.spec_adult-001-night-bg.in.log) \
+        <(grep "result" reversed_towards-001-rev.spec_adult-001-night-bg.in.log)
+fi
+if [ -f qtrlwe2_towards-002.spec_adult-001-night-bg.in.log ]; then
+    echo -n "."
+    diff \
+        <(grep "result" plain_towards-002.spec_adult-001-night-bg.in.log) \
+        <(grep "result" qtrlwe2_towards-002.spec_adult-001-night-bg.in.log)
+fi
+if [ -f reversed_towards-002.spec_adult-001-night-bg.in.log ]; then
+    echo -n "."
+    diff \
+        <(grep "result" plain_towards-002.spec_adult-001-night-bg.in.log) \
+        <(grep "result" reversed_towards-002-rev.spec_adult-001-night-bg.in.log)
+fi
+if [ -f qtrlwe2_towards-004.spec_adult-001-night-bg.in.log ]; then
+    echo -n "."
+    diff \
+        <(grep "result" plain_towards-004.spec_adult-001-night-bg.in.log) \
+        <(grep "result" qtrlwe2_towards-004.spec_adult-001-night-bg.in.log)
+fi
+if [ -f reversed_towards-004.spec_adult-001-night-bg.in.log ]; then
+    echo -n "."
+    diff \
+        <(grep "result" plain_towards-004.spec_adult-001-night-bg.in.log) \
+        <(grep "result" reversed_towards-004-rev.spec_adult-001-night-bg.in.log)
+fi
+if [ -f qtrlwe2_damon-001.spec_adult-001-7days-bg.in.log ]; then
+    echo -n "."
+    diff \
+        <(grep "result" plain_damon-001.spec_adult-001-7days-bg.in.log) \
+        <(grep "result" qtrlwe2_damon-001.spec_adult-001-7days-bg.in.log)
+fi
+if [ -f reversed_damon-001-rev.spec_adult-001-7days-bg.in.log ]; then
+    echo -n "."
+    diff \
+        <(grep "result" plain_damon-001.spec_adult-001-7days-bg.in.log) \
+        <(grep "result" reversed_damon-001-rev.spec_adult-001-7days-bg.in.log)
+fi
