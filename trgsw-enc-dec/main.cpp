890692dc3b1e41bbe414c0d65278cbf2717cd78d
Naoki MATSUMOTO <naoki@pibvt.net>
2022-05-19 13:30:44 +0900

    [trgsw-enc-dec] Output all measured times

--- trgsw-enc-dec/main.cpp
+++ trgsw-enc-dec/main.cpp
@@ -5,18 +5,20 @@ using namespace TFHEpp;
 
 const size_t NUM_TEST = 1000;
 
-void perform(const SecretKey &sk, std::vector<TRGSWFFT<lvl1param>> &cs);
+void perform(const SecretKey &sk, std::vector<TRGSWFFT<lvl1param>> &cs, std::vector<double> &elapsed);
 
 int main() {
   std::unique_ptr<SecretKey> sk = std::make_unique<SecretKey>();
   std::vector<TRGSWFFT<lvl1param>> cs(NUM_TEST);
+  std::vector<double> elapsed;
 
-  using namespace std::chrono;
-  system_clock::time_point start = chrono::system_clock::now();
-  perform(*sk, cs);
-  system_clock::time_point end = chrono::system_clock::now();
+  perform(*sk, cs, elapsed);
 
-  double elapsed = duration_cast<microseconds>(end - start).count();
-  std::cout << "Total: " << elapsed << "(us)\n"
-            << "Mean: " << elapsed / NUM_TEST << "(us)\n";
+  double elapsed_total = std::accumulate(elapsed.begin(), elapsed.end(), 0.0, [](double sum, double e) {
+    std::cout << e << std::endl;
+    return sum + e;
+  });
+
+  std::cout << "Total: " << elapsed_total << "(us)\n"
+            << "Mean: " << elapsed_total / NUM_TEST << "(us)\n";
 }
--- trgsw-enc-dec/proc.cpp
+++ trgsw-enc-dec/proc.cpp
@@ -1,10 +1,16 @@
 #include <tfhe++.hpp>
+#include <chrono>
 
 using namespace TFHEpp;
 
-void perform(const SecretKey &sk, std::vector<TRGSWFFT<lvl1param>> &cs) {
+void perform(const SecretKey &sk, std::vector<TRGSWFFT<lvl1param>> &cs, std::vector<double> &elapsed) {
   Polynomial<lvl1param> poly = {};
   for (auto &&t : cs) {
+    std::chrono::system_clock::time_point start = chrono::system_clock::now();
     t = trgswfftSymEncrypt<lvl1param>(poly, lvl1param::α, sk.key.lvl1);
+    std::chrono::system_clock::time_point end = chrono::system_clock::now();
+
+    double e = duration_cast<std::chrono::microseconds>(end - start).count();
+    elapsed.push_back(e);
   }
 }
